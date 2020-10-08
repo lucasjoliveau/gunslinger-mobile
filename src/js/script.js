20bcc7f5202088843bfab2f20750c7593a20a136
Lucas Joliveau <joliveau.lucas@gmail.com>
2020-10-07 21:26:20 -0400

    Add home screen and game screen

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+
+    const selectElement = (element) => document.querySelector(element);
+
+    selectElement('.start-page-btn').addEventListener('click', () => {
+        selectElement('.start-page').classList.add('u-hidden');
+        selectElement('.game-page').classList.remove('u-hidden');
+    });
 });
